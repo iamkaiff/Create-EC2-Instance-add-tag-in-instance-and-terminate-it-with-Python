c561bfb49a23be82867452f50261ded1db9d412d
iamkaiff <60395455+iamkaiff@users.noreply.github.com>
2020-02-26 12:36:28 +0530

    Update create.py

--- create.py
+++ create.py
@@ -1,9 +1,11 @@
 import boto3
 client=boto3.client('ec2')
+#You can choose your particular ami and size according to your need and paste it ami id and size below in the code. 
 resp=client.run_instances(ImageId='ami-0d9462a653c34dab7',              
 				InstanceType='t2.micro',
 				MinCount=1,
 				MaxCount=1)
 for instance in resp['Instances']:
 	print(instance['InstanceId'])
-
+#After this your instance will be shown here with its respective id.
+#Your instance is created.
