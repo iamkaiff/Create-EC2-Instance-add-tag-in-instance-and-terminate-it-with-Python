4188ba9966371f79a8c80281c744b3cb34c02d48
iamkaiff <60395455+iamkaiff@users.noreply.github.com>
2020-02-26 12:40:36 +0530

    Update tag.py

--- tag.py
+++ tag.py
@@ -5,7 +5,7 @@ ec2 = boto3.client('ec2')
 
 
 instances = ec2.describe_instances()
-
+#Write the particular instance id which was generated at the time of creation in next line.
 ids = ['instance id]
 
 
@@ -15,8 +15,8 @@ ec2.create_tags(
     Resources=ids,
     Tags=[
         {
-            'Key': 'Name',
+            'Key': 'Name',                 #Give the particular key and value which you  want to give your instance.
             'Value': 'Value'
         }
     ]
-)
+)                                         #Tag is updated in the instance.
